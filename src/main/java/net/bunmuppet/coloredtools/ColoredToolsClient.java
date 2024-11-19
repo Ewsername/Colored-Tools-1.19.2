b8632bb05bea34ee9667604095134d7f65d69f52
Ewsername <159178256+Ewsername@users.noreply.github.com>
2024-11-19 18:02:19 +0100

    Important initialization.

---
+++ src/main/java/net/bunmuppet/coloredtools/ColoredToolsClient.java
@@ -0,0 +1,10 @@
+package net.bunmuppet.coloredtools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColoredToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bunmuppet/coloredtools/ColoredToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bunmuppet.coloredtools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ColoredToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
