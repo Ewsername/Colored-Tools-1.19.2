33e0d04a50ae142463c11d47a06e03ffabb39f33
Ewsername <159178256+Ewsername@users.noreply.github.com>
2024-11-20 19:24:31 +0100

    Added custom tools to test the design.

--- src/main/java/net/bunmuppet/coloredtools/item/Moditems.java
+++ src/main/java/net/bunmuppet/coloredtools/item/Moditems.java
@@ -9,15 +9,105 @@ import net.minecraft.util.registry.Registry;
 
 public class Moditems {
 
+    //BIRCH TOOLS
     public static final Item WOODEN_BIRCH_PICKAXE = registerItem("wooden_birch_pickaxe",
             new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
     public static final Item WOODEN_BIRCH_SWORD = registerItem("wooden_birch_sword",
             new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_BIRCH_AXE = registerItem("wooden_birch_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_BIRCH_HOE = registerItem("wooden_birch_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_BIRCH_SHOVEL = registerItem("wooden_birch_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //ACACIA TOOLS
+    public static final Item WOODEN_ACACIA_PICKAXE = registerItem("wooden_acacia_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_ACACIA_SWORD = registerItem("wooden_acacia_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_ACACIA_AXE = registerItem("wooden_acacia_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_ACACIA_HOE = registerItem("wooden_acacia_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_ACACIA_SHOVEL = registerItem("wooden_acacia_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //SPRUCE TOOLS
+    public static final Item WOODEN_SPRUCE_PICKAXE = registerItem("wooden_spruce_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_SPRUCE_SWORD = registerItem("wooden_spruce_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_SPRUCE_AXE = registerItem("wooden_spruce_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_SPRUCE_HOE = registerItem("wooden_spruce_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_SPRUCE_SHOVEL = registerItem("wooden_spruce_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //CRIMSON TOOLS
+    public static final Item WOODEN_CRIMSON_PICKAXE = registerItem("wooden_crimson_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_CRIMSON_SWORD = registerItem("wooden_crimson_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_CRIMSON_AXE = registerItem("wooden_crimson_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_CRIMSON_HOE = registerItem("wooden_crimson_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_CRIMSON_SHOVEL = registerItem("wooden_crimson_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //DARK OAK TOOLS
+    public static final Item WOODEN_DARK_OAK_PICKAXE = registerItem("wooden_dark_oak_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_DARK_OAK_SWORD = registerItem("wooden_dark_oak_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_DARK_OAK_AXE = registerItem("wooden_dark_oak_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_DARK_OAK_HOE = registerItem("wooden_dark_oak_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_DARK_OAK_SHOVEL = registerItem("wooden_dark_oak_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //JUNGLE TOOLS
+    public static final Item WOODEN_JUNGLE_PICKAXE = registerItem("wooden_jungle_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_JUNGLE_SWORD = registerItem("wooden_jungle_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_JUNGLE_AXE = registerItem("wooden_jungle_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_JUNGLE_HOE = registerItem("wooden_jungle_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_JUNGLE_SHOVEL = registerItem("wooden_jungle_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //MANGROVE TOOLS
+    public static final Item WOODEN_MANGROVE_PICKAXE = registerItem("wooden_mangrove_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_MANGROVE_SWORD = registerItem("wooden_mangrove_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_MANGROVE_AXE = registerItem("wooden_mangrove_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_MANGROVE_HOE = registerItem("wooden_mangrove_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_MANGROVE_SHOVEL = registerItem("wooden_mangrove_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //WARPED TOOLS
+    public static final Item WOODEN_WARPED_PICKAXE = registerItem("wooden_warped_pickaxe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_WARPED_SWORD = registerItem("wooden_warped_sword",
+            new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item WOODEN_WARPED_AXE = registerItem("wooden_warped_axe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_WARPED_HOE = registerItem("wooden_warped_hoe",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+    public static final Item WOODEN_WARPED_SHOVEL = registerItem("wooden_warped_shovel",
+            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ColoredTools.MOD_ID, name), item);
     }
-
     public static void registerModItems()   {
         ColoredTools.LOGGER.debug("Registering Mod Items for" + ColoredTools.MOD_ID);
 
